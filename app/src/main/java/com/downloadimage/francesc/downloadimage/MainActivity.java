c3223278f296ce8c67e740631f1be76898d2f13c
Francesc Fores <francescfores@iesebre.com>
2016-01-10 23:34:43 +0100

    Download iamge Worker thread

---
+++ app/src/main/java/com/downloadimage/francesc/downloadimage/MainActivity.java
@@ -0,0 +1,57 @@
+package com.downloadimage.francesc.downloadimage;
+
+import android.app.ProgressDialog;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.view.View.OnClickListener;
+import java.io.InputStream;
+import java.net.URL;
+
+public class MainActivity extends ActionBarActivity {
+
+
+
+    private ImageView mImageView;
+    String URL = "http://www.androidbegin.com/wp-content/uploads/2013/07/HD-Logo.gif";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mImageView = (ImageView) findViewById(R.id.image);
+
+    }
+
+    private Bitmap loadImageFromNetwork(String url){
+        try {
+            Bitmap bitmap = BitmapFactory.decodeStream((InputStream) new URL(url).getContent());
+            return bitmap;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void onClick(View v) {
+        new Thread(new Runnable() {
+            public void run() {
+                final Bitmap bitmap = loadImageFromNetwork(URL);
+                mImageView.post(new Runnable() {
+                    public void run() {
+                        mImageView.setImageBitmap(bitmap);
+                    }
+                });
+            }
+        }).start();
+
+    }
+}
